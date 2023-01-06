e3ebab848a9b8145091b1322cfd26d8bf5619a49
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2023-01-06 14:24:12 -0300

    status bar

--- App.tsx
+++ App.tsx
@@ -1,18 +1,23 @@
-import { Groups } from '@screens/Groups';
 import { ThemeProvider } from 'styled-components';
-import theme from './src/theme';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto"
-import { LoadIndicator } from '@components/Loading/styles';
 import { Loading } from '@components/Loading';
+import theme from './src/theme';
+import { Groups } from '@screens/Groups';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
-    Roboto_700Bold
+    Roboto_700Bold 
   })
   return (
     <ThemeProvider theme={theme}>
-      {!fontsLoaded ? <Groups /> : <Loading />}
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
