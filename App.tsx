910611d00d8be74f6a52529ae4afc385bf7d98ee
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2023-01-06 14:09:10 -0300

    create component loading

--- App.tsx
+++ App.tsx
@@ -2,7 +2,8 @@ import { Groups } from '@screens/Groups';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/theme';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto"
-import { ActivityIndicator } from 'react-native';
+import { LoadIndicator } from '@components/Loading/styles';
+import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -11,7 +12,7 @@ export default function App() {
   })
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      {!fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
