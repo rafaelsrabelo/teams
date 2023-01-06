f69023c08959d57f9beb38c405defdd4c9769035
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2023-01-06 11:22:35 -0300

    create theme

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { Groups } from '@screens/Groups';
+import { ThemeProvider } from 'styled-components';
+import theme from './src/theme';
 
 export default function App() {
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      <Groups />
+    </ThemeProvider>
   );
 }
 
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react';
-import { Container, Title } from "./styles";
+import { Container } from "./styles";
 
 export function Groups() {
-  const [name, setName] = useState('Rafael Santana Rabelo')
   return (
     <Container>
-      <Title>Olá, {name}</Title>
     </Container>
   )
 }
