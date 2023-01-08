da4808cf12e08d46c25338f7dae7015cf66b9f8c
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2023-01-08 18:43:31 -0300

    create component button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,19 @@
+import { Container, Title, ButtonTypeStyleProps } from "./styles";
+import { TouchableOpacityProps } from 'react-native';
+
+type Props = {
+  title: string;
+  type?: ButtonTypeStyleProps;
+}
+
+export function Button({ title, type = 'PRIMARY', ...rest}: Props) {
+  return (
+    <Container
+      type={type}
+      {...rest}>
+      <Title>
+        {title}
+      </Title>
+    </Container>
+  )
+}
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Container } from "./styles";
 import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([])
@@ -23,7 +24,7 @@ export function Groups() {
           <ListEmpty message="Que tal cadastrar o primeiro time?" />
         )}
       />
-      
+      <Button title="Criar nova equipe" />
     </Container>
   )
 }
